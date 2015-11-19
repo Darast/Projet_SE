f9f65a12320fa33c6f7886541ccdf63b8f2ead47
s-mick <smick@ensc.fr>
2015-11-19 10:56:29 +0100

    Relecture et correction de vcd_writer.py

--- vcd_writer.py
+++ vcd_writer.py
@@ -1,5 +1,7 @@
-import serial # Package needed to manage data going through serial port
-import time
+import serial # Package needed to read data going through serial port
+
+import time   # Module needed to timestamp data
+import sys
 
 # Initialization phase
 vcd_file = open('acquired_data.vcd', 'w')
@@ -11,6 +13,8 @@ serialport.open()
 
 if(serialport.isOpen()):
 	print 'Serial port opened at', path, 'set at a baudrate of', baudrate, 'Bd'
+else:
+	sys.exit(1)
 
 # Write .vcd header
 date = time.strftime('%a %B %d %Y')
@@ -18,7 +22,7 @@ vcd_file.write('$date\n\t' + date + '\n$end\n'
 	+ '$timescale 1us $end\n$scope module logic $end\n')
 
 nlines = 2 # The number of line cannot exceed 8
-syms = ['#', '$', '%', '&', '(', '?', '§', '@'] # Symbols used to refer to the input wires
+syms = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'] # Symbols used to refer to the input wires
 names = ['databit', 'clock', 'l2', 'l3', 'l4', 'l5', 'l6', 'l7'] # Context-linked names associated to each input wire
 for i in range(nlines):
 	vcd_file.write('$var wire 1 ' + syms[i] + ' ' + names[i] + ' $end\n')
@@ -29,19 +33,23 @@ vcd_file.write('$upscope $end\n$enddefinitions $end\n')
 
 # # Reading loop
 
-# Default starting values are zeros
+# Default values are all zeros
 old_data = 0
-vcd_file.write('#0\n')
+vcd_file.write('$dumpvars\n')
 for i in range(nlines):
 	vcd_file.write('0'+ syms[i] + '\n')
 
-# Starting time
-start = time.clock()
-
 # Scrutinize the data going through the serial port and write them down on the .vcd file
+first_byte = True
 while(serialport.isOpen()):
 	curr_data = serialport.read(1) # Read exactly one byte from the serial port buffer
-	timediff = time.clock() - start
+
+	if (first_byte):
+		start = time.clock()
+		timediff = 0
+		first_byte = False
+	else:
+		 timediff = time.clock() - start
 
 	if (curr_data != old_data): # Check if the global data has changed
 		vcd_file.write('#' + str(int(1e6 * timediff)) + '\n')
@@ -50,12 +58,11 @@ while(serialport.isOpen()):
 			curr_ibit = (curr_data & (1 << i) ) >> i # Extract the i-th bit from the current and old data bytes
 			old_ibit = (old_data & (1 << i) ) >> i
 			if curr_ibit != old_ibit:
-				vcd_file.write(str(curr_ibit) + syms[i] + '\n')
+				vcd_file.write(str(curr_ibit) + ' ' + syms[i] + '\n')
 
 		old_data = curr_data # Ultimately, update memory of previously received data
 
 
 # Termination phase
-timestamp = str(int(1e6 * time.clock() - start) )
-vcd_file.write('#' + timestamp)
+vcd_file.write('#' + str(int(1e6 * (time.clock() - start) ) ) )
 vcd_file.close()
